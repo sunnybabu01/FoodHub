ba0d7edb14fe4b99c2f71c103ab2cc48bc5f0df9
Sunny Ranjan <sunnyranjan0448@gmail.com>
2026-02-26 15:53:00 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
-export const serverUrl="https://food-delivery-backend-175m.onrender.com"
+export const serverUrl="https://foodhub-qvfw.onrender.com"
 function App() {
     const {userData}=useSelector(state=>state.user)
     const dispatch=useDispatch()
